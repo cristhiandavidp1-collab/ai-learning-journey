7788fa8f00aa16b613996f03befcb619ca978756
Cristhian Peña <cristhiandavid.p1@gmail.com>
2026-04-01 14:44:52 -0500

    feat: add lists and loops practice with methods

---
+++ 01-python-fundamentals/04_list_practice.py
@@ -0,0 +1,28 @@
+# practice: list
+
+fruits = ["apple", "banana", "cherry"]
+
+# print(fruits[0])
+# print(fruits[1])
+# print(fruits[2])
+
+# loop through the list
+
+# for fruit in fruits:
+#    print(fruit)
+    
+# loop with conditional
+# numbers = [5, 12, 3, 18, 7, 25, 9]
+
+# for number in numbers:
+#   if number > 10:
+#       print(number)
+
+# list methods
+fruits.append("mango")
+print(fruits)
+
+fruits.remove("banana")
+print(fruits)
+
+print(len(fruits))
